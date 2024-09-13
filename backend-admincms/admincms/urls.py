7de37bbb0494482b6b3addec8b7dd7e2e2c97528
tej-2206 <sasi.venkat@ascenddefi.com>
2024-09-13 18:11:11 +0530

    fetching and storing

--- backend-admincms/admincms/urls.py
+++ backend-admincms/admincms/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import AdminCMSView
+from .views import AdminCMSView, AccountTypeListView, CurrencyTypeListView
 
 urlpatterns = [
-    path('admincms/', AdminCMSView.as_view(), name='admincms-create'),
+    path('admincms/', AdminCMSView.as_view(), name='admincms-create'),  # For adding data
+    path('admincms/account/', AccountTypeListView.as_view(), name='admincms-account-types'),  # For fetching account types
+    path('admincms/currency/', CurrencyTypeListView.as_view(), name='admincms-currency-types'),  # For fetching currency types
 ]
